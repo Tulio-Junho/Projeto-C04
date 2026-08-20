a5b571446841dc6cd2f9840d6cdf28fe9e41bb1f
Kauã <kauaaparecidosilvamorais@gmail.com>
2026-08-20 08:11:57 -0300

    Hello World

---
+++ PokedexProjetoC04.cpp
@@ -0,0 +1,20 @@
+//=====================================================
+//                Projeto de Pokedex
+//=====================================================
+// Integrantes:
+// Kauã Aparecido Silva Morais - Ges - 756
+// Ana Clara Oliveira e Silva  - Ges - 867
+// Túlio César Alves Junho     - xxx - xxx
+// 
+//
+
+
+#include <iostream> 
+using namespace std;
+
+int main(){
+
+    cout << "Hello World" << endl;
+
+    return 0;
+}
